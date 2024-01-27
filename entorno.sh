122d691e48c5a6a860ae34b597d828c0f2b969cb
M0xX25 <andreseduardo.012@gmail.com>
2024-01-26 20:17:03 -0500

    Archivos de Configuracion Wallpaper y Otros

--- entorno.sh
+++ entorno.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Actualizar el sistema
 distro=$(lsb_release -is)
 
 if [ "$distro" == "Kali" ]; then
@@ -10,6 +9,12 @@ elif [ "$distro" == "Parrot" ]; then
 fi
 
 sudo apt install -y neovim kitty build-essential git vim libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev kitty libxinerama1 libxinerama-dev cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev libxinerama1 libxinerama-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev bspwm libpcre3 libpcre3-dev
+
+if [ "$distro" == "Kali" ]; then
+  sudo apt update && sudo apt upgrade -y
+elif [ "$distro" == "Parrot" ]; then
+  sudo apt update && parrot-upgrade -y
+fi
 
 cd ~
 rm -rf Documents Music Pictures Public Videos Documentos Música Público Vídeos
