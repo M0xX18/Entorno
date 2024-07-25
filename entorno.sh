d6d085ef7bb13ab8b801b7cd698dde60ab0435ab
M0xX25 <andreseduardo.012@gmail.com>
2024-07-25 07:43:48 -0500

    Se agrega banner y tiempo de espera

--- entorno.sh
+++ entorno.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+echo "
+███╗░░░███╗░█████╗░██╗░░██╗██╗░░██╗░░░░██╗███████╗███╗░░██╗██╗░░░██╗
+████╗░████║██╔══██╗╚██╗██╔╝╚██╗██╔╝░░░██╔╝██╔════╝████╗░██║██║░░░██║
+██╔████╔██║██║░░██║░╚███╔╝░░╚███╔╝░░░██╔╝░█████╗░░██╔██╗██║╚██╗░██╔╝
+██║╚██╔╝██║██║░░██║░██╔██╗░░██╔██╗░░██╔╝░░██╔══╝░░██║╚████║░╚████╔╝░
+██║░╚═╝░██║╚█████╔╝██╔╝╚██╗██╔╝╚██╗██╔╝░░░███████╗██║░╚███║░░╚██╔╝░░
+╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░╚══════╝╚═╝░░╚══╝░░░╚═╝░░░
+"
+
+sleep 2
+echo -e "\tEntorno - Script de instalacion automatica de entorno de trabajo."
+echo -e "\t\t𝐴𝑛𝑑𝑟𝑒𝑠 𝐺𝑎𝑟𝑐𝑖𝑎 (Aka. 𝐌𝟎𝐱𝐗)"
+sleep 3
+echo -e "\nIniciando instalacion..\n"
+sleep 4
+
 distro=$(lsb_release -is)
 
-echo -e "\e[1;34mActualizando y mejorando el sistema...\e[0m"
+echo -e "\e[1;34mActualizando el sistema...\e[0m"
 
 if [ "$distro" == "Kali" ]; then
   sudo apt update && sudo apt upgrade -y
@@ -11,6 +27,7 @@ elif [ "$distro" == "Parrot" ]; then
 fi
 
 echo -e "\e[1;34mInstalando dependencias necesarias...\e[0m"
+sleep 2
 sudo apt install -y neovim kitty build-essential git vim libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev kitty libxinerama1 libxinerama-dev cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev libxinerama1 libxinerama-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev bspwm libpcre3 libpcre3-dev libxinerama-dev
  
 if [ "$distro" == "Kali" ]; then
@@ -20,6 +37,7 @@ elif [ "$distro" == "Parrot" ]; then
 fi
 
 echo -e "\e[1;34mCreando y limpiando directorios...\e[0m"
+sleep 1
 cd ~
 rm -rf Documents Music Pictures Public Videos Documentos Música Público Vídeos
 mkdir Utilidades
@@ -28,22 +46,26 @@ mkdir WebScripts
 mkdir Other
 
 echo -e "\e[1;34mClonando repositorios...\e[0m"
+sleep 1
 git clone http://github.com/M0xX25/MyScripts.git
 cd WebScripts
 git clone  http://github.com/baskerville/bspwm.git
 git clone http://github.com/baskerville/sxhkd.git
 
 echo -e "\e[1;34mCompilando e instalando bspwm...\e[0m"
+sleep 1
 cd bspwm
 make || { echo -e "\e[1;31mFallo al compilar bspwm\e[0m"; exit 1; }
 sudo make install || { echo -e "\e[1;31mFallo al instalar bspwm\e[0m"; exit 1; }
 
 cd ../sxhkd
 echo -e "\e[1;34mCompilando e instalando sxhkd...\e[0m"
+sleep 1
 make || { echo -e "\e[1;31mFallo al compilar sxhkd\e[0m"; exit 1; }
 sudo make install || { echo -e "\e[1;31mFallo al instalar sxhkd\e[0m"; exit 1; }
 
 echo -e "\e[1;34mConfigurando bspwm y sxhkd...\e[0m"
+sleep 1
 mkdir -p ~/.config/{bspwm,sxhkd}
 chmod +x ../bspwm/examples/bspwmrc
 cp ../bspwm/examples/bspwmrc ~/.config/bspwm/
@@ -69,6 +91,7 @@ bspc node -z "$dir" "$x" "$y" || bspc node -z "$falldir" "$x" "$y"' > /home/m0xx
 chmod +x /home/m0xx/.config/bspwm/scripts/bspwm_resize
 
 echo -e "\e[1;34mClonando y compilando polybar...\e[0m"
+sleep 1
 cd ../../
 git clone --recursive https://github.com/polybar/polybar
 cd polybar/
@@ -79,6 +102,7 @@ make -j$(nproc) || { echo -e "\e[1;31mFallo al compilar polybar\e[0m"; exit 1; }
 sudo make install || { echo -e "\e[1;31mFallo al instalar polybar\e[0m"; exit 1; }
 
 echo -e "\e[1;34mClonando y compilando picom...\e[0m"
+sleep 1
 cd ../..
 git clone https://github.com/ibhagwan/picom.git
 cd picom/
@@ -88,17 +112,20 @@ ninja -C build || { echo -e "\e[1;31mFallo al compilar picom\e[0m"; exit 1; }
 sudo ninja -C build install || { echo -e "\e[1;31mFallo al instalar picom\e[0m"; exit 1; }
 
 echo -e "\e[1;34mCopiando archivos de configuración...\e[0m"
+sleep 1
 cd ~
-cp -r ./home-m0xx/ ~
-cp -r ./home-root/ /root
+cp -r /home/m0xx/Entorno/home-m0xx/* ~
+cp -r /home/m0xx/Entorno/home-root/* /root
 
 echo -e "\e[1;34mConfigurando powerlevel10k...\e[0m"
+sleep 1
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k/
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>/root/.zshrc
 
 echo -e "\e[1;34mCreando enlace simbólico para .zshrc de root...\e[0m"
+sleep 1
 sudo ln -sf /home/m0xx/.zshrc /root/.zshrc
 
 echo -e "\e[1;32mEl entorno fue completamente instalado en el dispositivo.\e[0m"
